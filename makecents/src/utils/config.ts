87b71e9e61d586c90411e087f9bf40ff8d87d73e
Peng Ying <peng@lightspark.com>
2025-01-27 10:19:58 -0800

    Initial empty commit

--- makecents/src/utils/config.ts
+++
@@ -1,5 +0,0 @@
-export const APP_IDENTITY_PUBKEY =
-  "npub1hf4dj426lt26x5z7d3rc9c95yu7qggr3nv30xkqctv4vru7wnrsq6vw3gz";
-export const NOSTR_RELAY = "wss://nos.lol";
-export const MAKE_CENTS_SOURCE =
-  "https://umas-web.cdn.prismic.io/umas-web/Z1i0F5bqstJ98R4E_LIGHTSPARK_V5.1_FINAL241017_1080-handbrake-.mp4";
