b060d46b317d358b66f3b9a114ff2277c9afb705
Brian Siao Tick Chong <bsiaotickchong@gmail.com>
2025-04-04 17:05:33 -0700

    Update app identity pub keys, add favicons (#8)

--- makecents/src/utils/config.ts
+++ makecents/src/utils/config.ts
@@ -1,5 +1,5 @@
 export const APP_IDENTITY_PUBKEY =
-  "npub1hf4dj426lt26x5z7d3rc9c95yu7qggr3nv30xkqctv4vru7wnrsq6vw3gz";
+  "npub1lu9tllecrg53nk6wqzum3yd3ks2wr3gfrejvv9zsmp63hunvg4dsueuedr";
 export const NOSTR_RELAY = "wss://nos.lol";
 export const MAKE_CENTS_SOURCE =
   "https://umas-web.cdn.prismic.io/umas-web/Z1i0F5bqstJ98R4E_LIGHTSPARK_V5.1_FINAL241017_1080-handbrake-.mp4";
