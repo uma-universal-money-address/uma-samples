5b858f77534a122c1a57b932485ed4494e0b80ea
Jeremy Klein <jklein24@gmail.com>
2025-04-11 00:56:48 -0700

    Try sending sats for sats wallets

--- pennywall/src/hooks/usePayToAddress.ts
+++ pennywall/src/hooks/usePayToAddress.ts
@@ -1,20 +1,40 @@
 "use client";
 
-import { useNwcRequester } from "@uma-sdk/uma-auth-client";
+import { useNwcRequester, GetInfoResponse } from "@uma-sdk/uma-auth-client";
+import { useGetInfo } from "./useGetInfo";
+import { useState } from "react";
 
 export const usePayToAddress = () => {
   const { nwcRequester } = useNwcRequester();
+  const { getInfo } = useGetInfo();
+  const [info, setInfo] = useState<GetInfoResponse | undefined>();
 
-  const payToAddress = async (amountCents: number) => {
+  const payToAddress = async (amountCents: number, btcPrice: number) => {
     if (!nwcRequester) {
       console.warn("No NwcRequester available.");
       return;
     }
 
+    if (!info) {
+      const info = await getInfo();
+      setInfo(info);
+    }
+
+    const infoCurrencies = info?.currencies ?? [];
+    const sendAsSats =
+      infoCurrencies.length === 0 || infoCurrencies[0]?.currency.code === "SAT";
+    let amountToSend = amountCents;
+    if (sendAsSats) {
+      const amountInSats = Math.round(
+        (amountCents / 100) * btcPrice * 10000000
+      );
+      amountToSend = amountInSats * 1000; // Sends as msats
+    }
+
     return await nwcRequester.payToAddress({
       receiver: { lud16: "$fun-star-496@test.uma.me" },
-      sending_currency_code: "USD",
-      sending_currency_amount: amountCents,
+      sending_currency_code: sendAsSats ? "SAT" : "USD",
+      sending_currency_amount: amountToSend,
     });
   };
 
